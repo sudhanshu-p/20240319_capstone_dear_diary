cc1ace0011988bf6287d39e2d1e5dfa79d66c76d
sudhanshu-p <pandeysv@rknec.edu>
2024-04-03 17:42:55 +0530

    Merge conflicts resolved

--- 06_frontend/src/app/services/uploadimg.service.ts
+++ 06_frontend/src/app/services/uploadimg.service.ts
@@ -15,27 +15,6 @@ export class UploadService {
       const storageRef = ref(storage, `images/${Date.now()}_${file.name}`);
       const uploadTask = uploadBytesResumable(storageRef, file);
 
-<<<<<<< HEAD
-    uploadTask.on('state_changed', 
-      (snapshot) => {
-        // Optional: Observe state change events such as progress, pause, and resume
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-      }, 
-      (error) => {
-        // Handle unsuccessful uploads
-        console.error('Upload failed', error);
-      }, 
-      () => {
-        // Handle successful uploads on complete
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          console.log('File available at', downloadURL);
-
-          // Here, you can call a method to save the URL to your backend or do something else with it
-        });
-      }
-    );
-=======
       uploadTask.on('state_changed', 
         (snapshot) => {
           // Optional: Observe state change events such as progress, pause, and resume
@@ -57,6 +36,5 @@ export class UploadService {
         }
       );
     });
->>>>>>> d1286b87d5efd562c15dff8c6870b8c5eecbdc85
   }
 }
